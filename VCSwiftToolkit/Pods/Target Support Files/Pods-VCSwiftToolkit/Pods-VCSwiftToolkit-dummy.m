e8bc1a9c154f60fc5f365a7b50c58618ada6324f
Vitor Cesco <vitorcesco@gmail.com>
2017-04-17 13:19:55 -0300

    fixed untracked files

--- VCSwiftToolkit/Pods/Target Support Files/Pods-VCSwiftToolkit/Pods-VCSwiftToolkit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VCSwiftToolkit : NSObject
-@end
-@implementation PodsDummy_Pods_VCSwiftToolkit
-@end
--- VCSwiftToolkit/Pods/Target Support Files/Pods-VCSwiftToolkitTests/Pods-VCSwiftToolkitTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VCSwiftToolkitTests : NSObject
-@end
-@implementation PodsDummy_Pods_VCSwiftToolkitTests
-@end
